b850451bb8c9b8a1950a04fbde57d9c3915f1770
Manna Kifle <88642439+QAcoder1@users.noreply.github.com>
2022-06-29 19:50:12 -0400

    Add files via upload

---
+++ src/test/java/com/shoes_market/api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoes_market.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
